85058bcea57b047e1156144fa0c011ddd2d1ddc3
Frost-ZX <frost-zx@foxmail.com>
2023-12-02 13:37:15 +0800

    feat(javascript): 优化 getHEXColor，支持 Alpha 通道

--- javascript/common/color/getHEXColor.d.ts
+++ javascript/common/color/getHEXColor.d.ts
@@ -1,4 +1,9 @@
-/** 获取随机的十六进制颜色值 */
-declare function getHEXColor(): string;
+/**
+ * @description 获取随机的十六进制颜色值
+ * - 返回值默认格式：`#RRGGBB`
+ * - 若包含 Alpha 通道，返回值格式为：`#RRGGBBAA`
+ * @param alpha 是否包含 Alpha 通道，若为字符串则使用指定值
+ */
+declare function getHEXColor(alpha: boolean | string): string;
 
 export default getHEXColor;
--- javascript/common/color/getHEXColor.js
+++ javascript/common/color/getHEXColor.js
@@ -1,16 +1,25 @@
 /** @type { import('./getHEXColor')['default'] } */
-function getHEXColor() {
+function getHEXColor(alpha = false) {
+
+  let chars = ['#'];
+  let index = 0;
+  let length = (alpha === true ? 8 : 6);
   let list = [
     '0', '1', '2', '3', '4', '5', '6', '7',
     '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
   ];
-  let chars = ['#'];
-  let index = 0;
-  for (let i = 0; i < 6; i++) {
+
+  for (let i = 0; i < length; i++) {
     index = Math.floor(Math.random() * 16);
     chars.push(list[index]);
   }
+
+  if (typeof alpha === 'string') {
+    chars.push(alpha);
+  }
+
   return chars.join('');
+
 }
 
 export default getHEXColor;
