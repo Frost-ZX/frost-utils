1c4c4db81e627e5478a782af70f52ccc47cea8e4
Frost-ZX <frost-zx@foxmail.com>
2024-04-08 23:43:30 +0800

    fix(javascript): 修改 .d.ts 文件内的 import 路径中的 .js 为 .d.ts

--- javascript/browser/index.d.ts
+++ javascript/browser/index.d.ts
@@ -1,8 +1,8 @@
-export type { default as isElement } from './isElement.js';
-export type { default as isHTMLElement } from './isHTMLElement.js';
-export type { default as observeElementVisible } from './observeElementVisible.js';
-export type { default as rectCollisionCheck } from './rectCollisionCheck.js';
+export type { default as isElement } from './isElement.d.ts';
+export type { default as isHTMLElement } from './isHTMLElement.d.ts';
+export type { default as observeElementVisible } from './observeElementVisible.d.ts';
+export type { default as rectCollisionCheck } from './rectCollisionCheck.d.ts';
 
-export type { default as blobToDataURL } from './data-url/blobToDataURL.js';
-export type { default as requestDataURL } from './data-url/requestDataURL.js';
-export type { default as selectFileDataURL } from './data-url/selectFileDataURL.js';
+export type { default as blobToDataURL } from './data-url/blobToDataURL.d.ts';
+export type { default as requestDataURL } from './data-url/requestDataURL.d.ts';
+export type { default as selectFileDataURL } from './data-url/selectFileDataURL.d.ts';
